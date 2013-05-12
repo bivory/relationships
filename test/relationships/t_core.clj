c42dd2108f95f404496bbc4de2cfbc7dfcb8a942
Bryan Ivory <bivory@gmail.com>
2013-05-11 17:56:55 -0700

    test case: another multiline relationship

--- test/relationships/t_core.clj
+++ test/relationships/t_core.clj
@@ -10,6 +10,12 @@
 
   (fact "can parse multi-line relationships"
         (parse-relations "John,Sue\nMary,Fred\n") => [{:parent "John" :child "Sue"}
-                                                      {:parent "Mary" :child "Fred"}])
+                                                      {:parent "Mary" :child "Fred"}]
+        (parse-relations "John,Sue\nMary,Fred\nMary,Sue\nSue,Sam")
+        => [{:parent "John" :child "Sue"}
+            {:parent "Mary" :child "Fred"}
+            {:parent "Mary" :child "Sue"}
+            {:parent "Sue" :child "Sam"}])
+
   (fact "can read in files"))
 
