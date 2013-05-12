f5c37b8f0db41149e61eff90753f41d7000130f2
Bryan Ivory <bivory@gmail.com>
2013-05-12 12:11:46 -0700

    Fixed the lein-midje plugin dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject relationships "0.0.1-SNAPSHOT"
   :description "Model crazy family relationships."
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]
-                                  [lein-midje "3.0.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]
+                   :plugins [[lein-midje "3.0.1"]]}})
