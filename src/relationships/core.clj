c49348282cf75d7fb8a053650b8b0877948e7a77
Bryan Ivory <bivory@gmail.com>
2013-05-11 17:35:50 -0700

    Created the procotol

--- src/relationships/core.clj
+++ src/relationships/core.clj
@@ -1,10 +1,17 @@
 (ns relationships.core)
 
-;;; This is an incorrect implementation, such as might be written by
-;;; someone who was used to a Lisp in which an empty list is equal to
-;;; nil.
-(defn first-element [sequence default]
-  (if (nil? sequence)
-    default
-    (first sequence)))
-
+(defprotocol FamilyTree
+  "A protocol that allows the following queries about a family structure:"
+  (isParent?
+    [parent child]
+    "This returns true if the parent is a parent of the child.")
+  (isSibling?
+    [person1 person2]
+    "This returns true if person1 and person2 have at least one common parent.")
+  (isAncestor?
+    [person1 person2]
+    "This returns true if person1 is an ancestor of person2.")
+  (isRelated?
+    [person1 person2]
+    "This returns true if person1 and person2 have at least one common ancestor.
+     This would mean that siblings satisfy this relations as well."))
