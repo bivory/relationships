e8bdf5da935a053172363a9001df1531ad7f3719
Bryan Ivory <bivory@gmail.com>
2013-05-15 15:10:32 -0700

    Cloverage for coverage

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Model crazy family relationships."
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[midje "1.5.0"]]
-                   :plugins [[lein-midje "3.0.1"]]}})
+                   :plugins [[lein-midje "3.0.1"]
+                             [lein-cloverage "1.0.2"]]}})
